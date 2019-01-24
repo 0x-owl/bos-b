1fc77a34a2ccc40a80af6096f3cce81dc4ac98f7
Tom <tvukasovic@outlook.com>
2019-01-23 22:23:44 -0300

    Remove unecessary constants

--- coc/creator/constants.py
+++ coc/creator/constants.py
@@ -2,16 +2,3 @@ GENDER = (
     ('M', 'Male'),
     ('F', 'Female')
 )
-
-
-ATTR = (
-    ('STR', 'Strength'),
-    ('DEX', 'Dexterity'),
-    ('APP', 'Appearence'),
-    ('EDU', 'Education'),
-    ('INT', 'Intelligence'),
-    ('SIZ', 'Size'),
-    ('CON', 'Constitution'),
-    ('LCK', 'Luck'),
-    ('PWR', 'Power')
-)
