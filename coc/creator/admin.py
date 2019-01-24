97ed10cc8fe365c8575933f80c97e561d88c638d
Tom <tvukasovic@outlook.com>
2019-01-23 22:23:31 -0300

    Register new tag model

--- coc/creator/admin.py
+++ coc/creator/admin.py
@@ -1,13 +1,14 @@
 from creator.models import (Investigator, InvestigatorAttribute,
-                            InvestigatorSkills, Occupation, OccupationAttribute,
-                            OccupationSkills, Skills)
+                            InvestigatorSkills, Occupation,
+                            OccupationAttribute, OccupationSkills, Skills, Tag)
 from django.contrib import admin
 
 # Register your models here.
 admin.site.register(Investigator)
-admin.site.register(Skills)
-admin.site.register(Occupation)
 admin.site.register(InvestigatorAttribute)
 admin.site.register(InvestigatorSkills)
+admin.site.register(Occupation)
 admin.site.register(OccupationAttribute)
 admin.site.register(OccupationSkills)
+admin.site.register(Skills)
+admin.site.register(Tag)
