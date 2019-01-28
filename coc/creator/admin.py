13fdfe7fb93cfed870f8788f52f8fc3df20310c2
Tom <tvukasovic@outlook.com>
2019-01-28 00:09:00 -0300

    Register tagging related models

--- coc/creator/admin.py
+++ coc/creator/admin.py
@@ -1,14 +1,18 @@
 from creator.models import (Investigator, InvestigatorAttribute,
-                            InvestigatorSkills, Occupation,
-                            OccupationAttribute, OccupationSkills, Skills, Tag)
+                            InvestigatorSkills, InvestigatorTags, Occupation,
+                            OccupationAttribute, OccupationSkills,
+                            OccupationTags, SkillTags, Skills, Tag)
 from django.contrib import admin
 
 # Register your models here.
 admin.site.register(Investigator)
 admin.site.register(InvestigatorAttribute)
 admin.site.register(InvestigatorSkills)
+admin.site.register(InvestigatorTags)
 admin.site.register(Occupation)
 admin.site.register(OccupationAttribute)
 admin.site.register(OccupationSkills)
+admin.site.register(OccupationTags)
 admin.site.register(Skills)
+admin.site.register(SkillTags)
 admin.site.register(Tag)
