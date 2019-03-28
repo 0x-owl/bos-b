c50e049722428347e9b0ee4bb16f7d0dfac6b322
Tom <tvukasovic@outlook.com>
2019-03-27 22:07:55 -0300

    Grapql api graphene urls

--- coc/coc/urls.py
+++ coc/coc/urls.py
@@ -18,6 +18,10 @@ from django.conf.urls.static import static
 from django.urls import path
 from django.contrib import admin
 
+from graphene_django.views import GraphQLView
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('graphql', GraphQLView.as_view(graphiql=True)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
