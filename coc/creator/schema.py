e916462d3c392539af81fc4f48ab5ddc9e98c6b6
Tom <tvukasovic@outlook.com>
2019-03-29 17:42:23 -0300

    Add schema for basic queries Tag

--- coc/creator/schema.py
+++ coc/creator/schema.py
@@ -1,7 +1,7 @@
-from graphene import List
+from graphene import List, Field, Int, String
 from graphene_django.types import DjangoObjectType
 
-from creator.models import Item
+from creator.models import Item, Tag
 
 
 class ItemType(DjangoObjectType):
@@ -9,8 +9,43 @@ class ItemType(DjangoObjectType):
         model = Item
 
 
+class TagType(DjangoObjectType):
+    class Meta:
+        model = Tag
+
+
 class Query(object):
     all_items = List(ItemType)
+    all_tags = List(TagType)
+    tag = Field(
+        TagType,
+        pk=Int(),
+        title=String(),
+        uuid=String()
+    )
 
     def resolve_all_items(self, info, **kwargs):
         return Item.objects.all()
+
+    def resolve_all_tags(self, info, **kwargs):
+        return Tag.objects.all()
+
+    def resolve_tag(self, info, **kwargs):
+        pk = kwargs.get('pk')
+        uuid = kwargs.get('uuid')
+        title = kwargs.get('title')
+        ret = None
+
+        if pk is not None:
+            tag = Tag.objects.get(pk=pk)
+            ret = tag
+
+        if uuid is not None:
+            tag = Tag.objects.get(uuid=uuid)
+            ret = tag
+
+        if title is not None:
+            tag = Tag.objects.get(title=title)
+            ret = tag
+
+        return ret
