08ccc26771f0491a9a5a7a748aa6263c21ab7cf6
Tom <tvukasovic@outlook.com>
2019-03-29 22:08:37 -0300

    simplified resolve tag method.

--- coc/creator/schema.py
+++ coc/creator/schema.py
@@ -1,7 +1,9 @@
-from graphene import List, Field, Int, String
+from graphene import Field, List
 from graphene_django.types import DjangoObjectType
 
 from creator.models import Item, Tag
+from creator.constants import TAG_FIELDS
+from creator.helpers.schema_helpers import attribute_resolver
 
 
 class ItemType(DjangoObjectType):
@@ -19,33 +21,17 @@ class Query(object):
     all_tags = List(TagType)
     tag = Field(
         TagType,
-        pk=Int(),
-        title=String(),
-        uuid=String()
+        **TAG_FIELDS
     )
 
     def resolve_all_items(self, info, **kwargs):
+        """Obtain all records from Item model."""
         return Item.objects.all()
 
     def resolve_all_tags(self, info, **kwargs):
+        """Obtain all records from Tag model."""
         return Tag.objects.all()
 
     def resolve_tag(self, info, **kwargs):
-        pk = kwargs.get('pk')
-        uuid = kwargs.get('uuid')
-        title = kwargs.get('title')
-        ret = None
-
-        if pk is not None:
-            tag = Tag.objects.get(pk=pk)
-            ret = tag
-
-        if uuid is not None:
-            tag = Tag.objects.get(uuid=uuid)
-            ret = tag
-
-        if title is not None:
-            tag = Tag.objects.get(title=title)
-            ret = tag
-
-        return ret
+        """Obtain record that matches specific query over fields."""
+        return attribute_resolver(Tag, TAG_FIELDS, kwargs)
