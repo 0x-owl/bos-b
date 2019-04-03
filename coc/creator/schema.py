7164f2bab0f41dbc590b48e14458edab8e0eb2d8
Tom <tvukasovic@outlook.com>
2019-04-02 23:26:56 -0300

    Add relay mutations for tag

--- coc/creator/schema.py
+++ coc/creator/schema.py
@@ -1,5 +1,5 @@
-from graphene import relay
-from creator.models import Tag, Portrait
+from graphene import ClientIDMutation, Field, Int, String, relay
+from creator.models import Portrait, Tag
 from django.contrib.auth.models import User
 
 from graphene_django.types import DjangoObjectType
@@ -28,6 +28,53 @@ class TagNode(DjangoObjectType):
         interfaces = (relay.Node, )
 
 
+class CreateTag(ClientIDMutation):
+    tag = Field(TagNode)
+
+    class Input:
+        title = String()
+        user = Int()
+
+    @classmethod
+    def mutate(cls, *args, **kwargs):
+        """Generates mutation which is an instance of the Node class which
+        results in a instance of our model.
+        Arguments:
+            input -- (dict) dictionary that has the keys corresponding to the
+            Input class (title, user).
+        """
+        arguments = kwargs.get('input')
+        tag = Tag(
+            title=arguments.get('title'),
+            user=User.objects.get(pk=arguments.get('user'))
+        )
+        tag.save()
+        return CreateTag(tag=tag)
+
+
+class UpdateDeleteTag(ClientIDMutation):
+    tag = Field(TagNode)
+
+    class Input:
+        title = String()
+        uuid = String()
+        method = String()
+
+    @classmethod
+    def mutate(cls, *args, **kwargs):
+        uuid = kwargs.get('input').get('uuid', '')
+        if uuid != '':
+            tag = Tag.objects.get(uuid=uuid)
+            if tag is not None and kwargs.get('input').get('method') != 'DEL':
+                tag.title = kwargs.get('input').get('title')
+                tag.save()
+                ret = UpdateDeleteTag(tag=tag)
+            else:
+                tag.delete()
+                ret = "Delete"
+            return ret
+
+
 class PortraitNode(DjangoObjectType):
     class Meta:
         model = Portrait
@@ -46,3 +93,8 @@ class Query(object):
 
     all_portraits = DjangoFilterConnectionField(PortraitNode)
     portrait = relay.Node.Field(PortraitNode)
+
+
+class Mutation(object):
+    new_tag = CreateTag.Field()
+    update_delete_tag = UpdateDeleteTag.Field()
