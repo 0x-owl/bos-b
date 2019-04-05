b02b11e9c24ffc1826a31182ad7f9c806289352f
Florencia Vela <flor.p.vela@gmail.com>
2019-04-05 19:40:45 +0000

    Merge branch '29_Phobias_and_Manias_Seeder' into 'development'
    manias and phobias seeders
    
    See merge request tomowl/coc!15

--- coc/coc/schema.py
+++ coc/coc/schema.py
@@ -1,9 +1,13 @@
 from graphene import ObjectType, Schema
-from creator.schema import Query as Q
+from creator.schema import Query as Q, Mutation as M
 
 
 class Query(Q, ObjectType):
     pass
 
 
-schema = Schema(query=Query)
+class Mutation(M, ObjectType):
+    pass
+
+
+schema = Schema(query=Query, mutation=Mutation)
