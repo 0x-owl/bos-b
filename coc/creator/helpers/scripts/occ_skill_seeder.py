c4bb939cc8b2cf0385bbc6b3958c14d2f42c79a3
Tom <tomas@thirdpartytrust.com>
2019-04-22 01:10:19 -0300

    Add script to generate entries for occupation skill

--- coc/creator/helpers/scripts/occ_skill_seeder.py
+++ coc/creator/helpers/scripts/occ_skill_seeder.py
@@ -2,16 +2,18 @@
 
 python3 creator/helpers/scripts/occ_skill_seeder.py -o Accountant -s
 "['Accounting', 'Law', 'Library Use', 'Listen', 'Persuade']"
+
+python3 creator/helpers/scripts/occ_skill_seeder.py -o Accountant -s
+"['Accounting', 'Law', 'Library Use', 'Listen', 'Persuade']" -op -c "4" -l 2
 """
-from os import environ
+from os import environ, system
 from ast import literal_eval as le
 from json import dumps
-from pprint import pprint as pp
 from argparse import ArgumentParser
 
 from django.core.wsgi import get_wsgi_application
 
-environ.setdefault("PYTHONPATH", "/coc/coc")
+system("export PYTHONPATH=/coc/coc")  # run this command at the bash
 environ.setdefault("DJANGO_SETTINGS_MODULE", "coc.settings")
 apps = [
     'django.contrib.admin',
@@ -23,7 +25,7 @@ apps = [
     'creator'
 ]
 environ.setdefault("INSTALLED_APPS", str(apps))
-application = get_wsgi_application()
+get_wsgi_application()
 
 from creator.models import Occupation, Skills
 
@@ -35,21 +37,31 @@ List of skills to search, if optional then this list will be
 excluded from optionals'''
                     )
 parser.add_argument(
-    '-op', '--optional', help='whether if we need a list of optional skills',
-    action='store_true')
+    '-op', '--optional', help='Whether if we need a list of optional skills',
+    action='store_true'
+)
+parser.add_argument(
+    '-c', '--category', help='Category of the skill (default="1")',
+    default="1"
+)
+parser.add_argument(
+    '-l', '--limit', help='Limit of optionals (default=0)',
+    default=0
+)
 args = parser.parse_args()
 
 skills = Skills.objects.all()
 occ = Occupation.objects.filter(title=args.occupation).first()
 skills_list = le(args.skills)
-
-seed = []
+out = ''
 initial_registry = {
         "model": "creator.OccupationSkills",
         "fields": {
             "occupation": None,
             "skill": None,
-            "optional": False
+            "optional": False,
+            "category": args.category,
+            "limit": args.limit
         }
 }
 
@@ -60,7 +72,7 @@ if args.optional:
         reg['fields']['skill'] = skill.pk
         reg['fields']['occupation'] = occ.pk
         reg['fields']['optional'] = True
-        seed.append(reg)
+        out += '{},\n'.format(dumps(reg))
 
 else:
     sks = [skill for skill in skills if skill.title in skills_list]
@@ -68,7 +80,6 @@ else:
         reg = initial_registry
         reg['fields']['skill'] = skill.pk
         reg['fields']['occupation'] = occ.pk
-        seed.append(reg)
+        out += '{},\n'.format(dumps(reg))
 
-
-pp(dumps(seed))
+print(out)
