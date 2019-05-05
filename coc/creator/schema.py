0c85930ff533627cbd714da0d68b1c997a314189
Eliseo Moreno <eliseomoreno13@gmail.com>
2019-05-05 16:29:14 +0000

    Schema investigator related model

--- coc/creator/schema.py
+++ coc/creator/schema.py
@@ -1,9 +1,12 @@
-from graphene import ClientIDMutation, Field, Float, Int, String, relay
-from creator.models import Occupation, Portrait, Tag, Item
+from creator.models import Investigator, Item, Occupation, Portrait, Tag
+
 from django.contrib.auth.models import User
 
-from graphene_django.types import DjangoObjectType
+from graphene import ClientIDMutation, Field, Float, Int, String, relay
+
 from graphene_django.filter import DjangoFilterConnectionField
+
+from graphene_django.types import DjangoObjectType
 
 
 class UserNode(DjangoObjectType):
@@ -86,7 +89,7 @@ class ItemNode(DjangoObjectType):
         filter_fields = {
             'uuid': ['exact'],
             'title': ['exact', 'icontains', 'istartswith'],
-            'item_type': ['exact'], 
+            'item_type': ['exact'],
             'price': ['exact', 'gt', 'lt', 'gte', 'lte']
         }
         interfaces = (relay.Node, )
@@ -168,7 +171,7 @@ class OccupationNode(DjangoObjectType):
             'user__username': ['exact', 'istartswith'],
             'user__id': ['exact'],
             'title': ['exact', 'icontains', 'istartswith'],
-            'suggested_contacts': ['icontains'], 
+            'suggested_contacts': ['icontains'],
             'credit_rating_min': ['exact', 'gt', 'lt', 'gte', 'lte'],
             'credit_rating_max': ['exact', 'gt', 'lt', 'gte', 'lte']
         }
@@ -218,7 +221,6 @@ class UpdateDeleteOccupation(ClientIDMutation):
 
     class Input:
         uuid = String()
-        user = Int()
         title = String()
         description = String()
         suggested_contacts = String()
@@ -239,15 +241,140 @@ class UpdateDeleteOccupation(ClientIDMutation):
         if uuid != '':
             occupation = Occupation.objects.get(uuid=uuid)
             if occupation is not None and method != 'DEL':
-                occupation.title = title 
-                occupation.description = description 
-                occupation.suggested_contacts = suggested_contacts 
-                occupation.credit_rating_min = credit_rating_min 
-                occupation.credit_rating_max = credit_rating_max 
+                occupation.title = title
+                occupation.description = description
+                occupation.suggested_contacts = suggested_contacts
+                occupation.credit_rating_min = credit_rating_min
+                occupation.credit_rating_max = credit_rating_max
                 occupation.save()
                 ret = UpdateDeleteOccupation(occupation=occupation)
             else:
                 occupation.delete()
+                ret = "Delete"
+            return ret
+
+
+class InvestigatorNode(DjangoObjectType):
+    class Meta:
+        model = Investigator
+        filter_fields = {
+            'uuid': ['exact'],
+            'user': ['exact'],
+            'user__username': ['exact', 'istartswith'],
+            'user__id': ['exact'],
+            'sex': ['exact'],
+            'age': ['exact', 'gt', 'lt', 'gte', 'lte'],
+            'occupation': ['exact']
+        }
+        interfaces = (relay.Node, )
+
+
+class CreateInvestigator(ClientIDMutation):
+    investigator = Field(InvestigatorNode)
+
+    class Input:
+        user = Int()
+        name = String()
+        player = String()
+        sex = String()
+        residence = String()
+        birthplace = String()
+        age = Int()
+        occupation = Int()
+        ideologies = String()
+        description = String()
+        traits = String()
+        injure_scars = String()
+        significant_people = String()
+        meaningful_locations = String()
+        treasured_possessions = String()
+        encounters_with_strange_entities = String()
+
+    @classmethod
+    def mutate(cls, *args, **kwargs):
+        """Generates mutation which is an instance of the Node class which
+        results in a instance of our model.
+        Arguments:
+            input -- (dict) dictionary that has the keys corresponding to the
+            Input class (title, user).
+        """
+        input_ = kwargs.get('input')
+        occ = Occupation.objects.get(pk=input_['occupation'])
+        usr = User.objects.get(pk=input_['user'])
+        input_['user'] = usr
+        input_['occupation'] = occ
+        investigator = Investigator(**input_)
+        investigator.save()
+        return CreateInvestigator(investigator=investigator)
+
+
+class UpdateDeleteInvestigator(ClientIDMutation):
+    investigator = Field(InvestigatorNode)
+
+    class Input:
+        uuid = String()
+        name = String()
+        player = String()
+        sex = String()
+        residence = String()
+        birthplace = String()
+        age = Int()
+        occupation = Int()
+        ideologies = String()
+        description = String()
+        traits = String()
+        injure_scars = String()
+        significant_people = String()
+        meaningful_locations = String()
+        treasured_possessions = String()
+        encounters_with_strange_entities = String()
+        method = String()
+
+    @classmethod
+    def mutate(cls, *args, **kwargs):
+        input_ = kwargs.get('input')
+        uuid = input_.get('uuid', '')
+        name = input_.get('name')
+        player = input_.get('player')
+        sex = input_.get('sex')
+        residence = input_.get('residence')
+        birthplace = input_.get('birthplace')
+        age = input_.get('age')
+        occupation = input_.get('occupation')
+        ideologies = input_.get('ideologies')
+        description = input_.get('description')
+        traits = input_.get('traits')
+        injure_scars = input_.get('injure_scars')
+        significant_people = input_.get('significant_people')
+        meaningful_locations = input_.get('meaningful_locations')
+        treasured_possessions = input_.get('treasured_possessions')
+        encounters_with_strange_entities = input_.get(
+            'encounters_with_strange_entities'
+        )
+        method = input_.get('method')
+        if uuid != '':
+            investigator = Investigator.objects.get(uuid=uuid)
+            if investigator is not None and method != 'DEL':
+                investigator.name = name
+                investigator.player = player
+                investigator.sex = sex
+                investigator.residence = residence
+                investigator.birthplace = birthplace
+                investigator.age = age
+                investigator.occupation = Occupation.objects.get(pk=occupation)
+                investigator.ideologies = ideologies
+                investigator.description = description
+                investigator.traits = traits
+                investigator.injure_scars = injure_scars
+                investigator.significant_people = significant_people
+                investigator.meaningful_locations = meaningful_locations
+                investigator.treasured_possessions = treasured_possessions
+                investigator.encounters_with_strange_entities =\
+                    encounters_with_strange_entities
+                investigator.save()
+                ret = UpdateDeleteInvestigator(investigator=investigator)
+            else:
+                investigator.delete()
                 ret = "Delete"
             return ret
 
@@ -276,7 +403,10 @@ class Query(object):
 
     all_occupations = DjangoFilterConnectionField(OccupationNode)
     occupation = relay.Node.Field(OccupationNode)
-    
+
+    all_investigators = DjangoFilterConnectionField(InvestigatorNode)
+    investigator = relay.Node.Field(InvestigatorNode)
+
 
 class Mutation(object):
     new_tag = CreateTag.Field()
@@ -285,3 +415,5 @@ class Mutation(object):
     update_delete_item = UpdateDeleteItem.Field()
     new_occupation = CreateOccupation.Field()
     update_delete_occupation = UpdateDeleteOccupation.Field()
+    new_investigator = CreateInvestigator.Field()
+    update_delete_investigator = UpdateDeleteInvestigator.Field()
