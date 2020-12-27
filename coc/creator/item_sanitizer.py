bfcd8a1d59aade1aa2d1d7f6ceeba2e148150931
Tom <tomowl@pm.me>
2020-12-26 21:32:29 -0600

    fix sanitizer

--- coc/creator/item_sanitizer.py
+++ coc/creator/item_sanitizer.py
@@ -1,31 +1,58 @@
-from coc.creator.models import Item
-from json import dumps
+import django
+from os import environ, path
+from json import dumps, loads
 
-items = Item.objects.all()
+from django.core.wsgi import get_wsgi_application
+environ.setdefault("DJANGO_SETTINGS_MODULE", "coc.settings")
+APPS = [
+    'django.contrib.contenttypes',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'creator'
+]
+django.setup()
+environ.setdefault("INSTALLED_APPS", str(APPS))
+app = get_wsgi_application()
+
+
+from coc.creator.models import Item
+from coc.creator.constants import ITEM_CATEGORIES
+from json import dumps
 
 
 def fixture_creator(item):
     age = item.description.splite(' - ')[-1]
     item = {
-        "category": item.category,
-        "properties": {
-            "title": item.title,
-            "description": item.description,
-            "price": item.price,
-            "age": age
+        "model": "creator.Item",
+        "fields": {
+            "category": item.category,
+            "properties": {
+                "title": item.properties['title'],
+                "description": item.properties['description'],
+                "price": item.properties['price'],
+                "age": item.properties['age']
+            }
         }
     }
     return item
 
+categories = dict(ITEM_CATEGORIES)
+items = Item.objects.all()
 
-with open('items.json', 'w') as fixture:
-    fixture.write('[\n')
-    last_item = items.pop(-1)
-    for item in items:
-        item_dump = fixture_creator(item)
-        fixture.write(f"{dumps(item_dump)},\n")
-    last_item_dump = fixture_creator(last_item)
-    fixture.write(f"{dumps(last_item_dump)}\n")
-    fixture.write(']')
+for category in categories:
+    items = Item.objects.filter(category=category)
+    if items:
+        with open(f'items_{categories[category].lower()}.json', 'w') as fixture:
+            fixture.write('[\n')
+            last_item = items.pop(-1)
+            for item in items:
+                item_dump = fixture_creator(item)
+                fixture.write(f"{dumps(item_dump)},\n")
+            last_item_dump = fixture_creator(last_item)
+            fixture.write(f"{dumps(last_item_dump)}\n")
+            fixture.write(']')
 
     
